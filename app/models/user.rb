4981627ad97c774f2ba857af43dd7847bd1c96db
takur-ryu <takuryu.1049@icloud.com>
2020-10-02 16:40:07 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  has_many :items
+  has_many :orders
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birth, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
