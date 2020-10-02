ee05c3f160c07d3ac4ee1b4460764c4b3fb3cf50
takur-ryu <takuryu.1049@icloud.com>
2020-10-02 18:58:41 +0900

    Update user.rb
    ①with_optionを用いて、まとめて
    空データ登録無効を適用化
    記述をリファクタリング
    ②first_name_kanaと
    last_name_kanaへ
    カタカナのみの正規表現適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth, presence: true
-
+  with_options presence: true do
+    validates :nickname
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してください!(お名前カナ(全角)※苗字"}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してください!(お名前カナ(全角)※名前"}
+    validates :birth
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
