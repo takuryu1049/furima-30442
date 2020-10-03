6417a4ca7e22da8cb9754cf36967478158b04978
takur-ryu <takuryu.1049@icloud.com>
2020-10-03 10:38:00 +0900

    ﾛｸﾞｲﾝ/ｱｳﾄ/ﾍﾟｰｼﾞ遷移設定等
    ○パラメータ受け取り誤字修正
    (アプリケーションコントローラー)
    ○会員登録：エラーメッセージバリデーション配置
    ○ログアウトボタン:パス設置
    ○送信用パラメータ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:last_name, :first_name_kane, :last_name_kana,:birth])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:last_name, :first_name_kana, :last_name_kana,:birth])
     end
 end
