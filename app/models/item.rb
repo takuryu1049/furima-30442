af328e6caba082b41e4a8e2e45064137353ab965
takur-ryu <takuryu.1049@icloud.com>
2020-10-04 15:08:21 +0900

    Itemﾓﾃﾞﾙ ﾊﾞﾘﾃﾞｰｼｮﾝ設定,ｱｿｼｴｰｼｮﾝ設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :stasus, :deliveryburden, :shipmentsource,  :daytoship, 
+  belongs_to :user
+  has_one :order
+
+  with_options presence: true do
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :status_id, numericality: { other_than: 1 } 
+    validates :delivery_burden_id, numericality: { other_than: 1 } 
+    validates :shipment_source_id, numericality: { other_than: 1 } 
+    validates :day_to_ship_id ,numericality: { other_than: 1 } 
+    validates :price, presence: true, numericality: {greater_than: 299, less_than: 10000000}, format: { with: /\A[0-9]+\z/ }
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな、全角カタカナ、漢字で入力必要(名前(全角)/苗字)' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな、全角カタカナ、漢字で入力必要(名前(全角)/名前)' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力必要(名前カナ(全角)/苗字)' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力必要(名前カナ(全角)/名前)' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字で入力必要(名前(全角)/苗字)"}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字で入力必要(名前(全角)/名前)"}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力必要(名前カナ(全角)/苗字)"}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力必要(名前カナ(全角)/名前)"}
     validates :birth
   end
   # Include default devise modules. Others available are:
