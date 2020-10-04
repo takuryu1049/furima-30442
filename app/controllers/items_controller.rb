ab9ca8d2eb8e3c2029452e29a4da49d2fbf16ab4
takur-ryu <takuryu.1049@icloud.com>
2020-10-04 23:42:46 +0900

    エラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,13 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_burden_id, :shipment_source_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :delivery_burden_id, :shipment_source_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
   end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,24 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :stasus, :deliveryburden, :shipmentsource,  :daytoship, 
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_burden
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :day_to_ship
   belongs_to :user
   has_one :order
   has_one_attached :image
 
   with_options presence: true do
-    validates :name, presence: true
-    validates :description, presence: true
+    validates :name
+    validates :description
     validates :category_id, numericality: { other_than: 1 } 
     validates :status_id, numericality: { other_than: 1 } 
     validates :delivery_burden_id, numericality: { other_than: 1 } 
-    validates :shipment_source_id, numericality: { other_than: 1 } 
+    validates :shipment_source_id, numericality: { other_than: 0 } 
     validates :day_to_ship_id ,numericality: { other_than: 1 } 
-    validates :price, presence: true, numericality: {greater_than: 299, less_than: 10000000}, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: {greater_than: 299, less_than: 10000000}, format: { with: /\A[0-9]+\z/ }
+    validates :image
   end
 
 end
