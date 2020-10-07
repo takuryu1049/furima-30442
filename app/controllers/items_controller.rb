e7f1c3376a0161fde0b7f13485696cbbf0a01637
takur-ryu <takuryu.1049@icloud.com>
2020-10-07 11:46:55 +0900

    出品した出品者以外編集ページ入れない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show,:edit]
   before_action :authenticate_user!, except:[:index,:show]
+  before_action :move_to_index, only: [:edit]
   
   def index
     @items = Item.all.order('created_at DESC')
@@ -44,5 +45,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :image, :name, :description, :category_id, :status_id, :delivery_burden_id, :shipment_source_id, :day_to_ship_id, :price
     ).merge(user_id: current_user.id)
+  end
+end
+
+def move_to_index
+  unless user_signed_in? && current_user.id ==  @item.user.id
+    redirect_to action: :index
   end
 end
