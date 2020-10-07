fa02135fb41386f9d25e77fa44475fb43b300903
takur-ryu <takuryu.1049@icloud.com>
2020-10-07 11:25:51 +0900

    authenticate_user!処理
    ログインしていなくても、一覧ページと詳細ページのみ許容

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show,:edit]
+  before_action :authenticate_user!, except:[:index,:show]
   
   def index
     @items = Item.all.order('created_at DESC')
