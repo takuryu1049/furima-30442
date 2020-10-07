dc5b58854dad3bdf21b3d52f4936687bc9ae9ceb
takur-ryu <takuryu.1049@icloud.com>
2020-10-07 10:12:10 +0900

    編集機能サーバーサイド実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,15 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  def update
+    @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(params[:id])
+      else
+        render :edit
+      end
+  end
+
   private
 
   def set_item
