fb8b5bb7976a25b29de197f4992209be85e1ee1a
takur-ryu <takuryu.1049@icloud.com>
2020-10-10 01:06:26 +0900

    複数テーブル保存処理実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,35 @@
 class OrdersController < ApplicationController
   def index
+    @order_delivery_address = OrderDeliveryAddress.new
+    @item = Item.find(params[:item_id])
   end
+  
+  def create
+    @order_delivery_address = OrderDeliveryAddress.new(order_delivery_address_params)
+    if @order_delivery_address.valid?
+      @order_delivery_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      inti_value(@order_delivery_address)
+      render :index
+    end
+  end
+
+  private
+
+  def order_delivery_address_params
+    params.require(:order_delivery_address).permit(:p_code,:prefecture_id,:cities,:address,:building_name,:phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def inti_value(order_delivery_address)
+    order_delivery_address.p_code = ""
+    order_delivery_address.prefecture_id = 0
+    order_delivery_address.cities = ""
+    order_delivery_address.address = ""
+    order_delivery_address.building_name = ""
+    order_delivery_address.phone_num = ""
+  end
+  # ※冗長かもしれないが今まで習った範囲だとこれしかないと思う。user_idとitem_idは初期化やっていないためセキュリティ面大丈夫か？。
+
 end
--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -1,6 +1,6 @@
-class UserDonation
+class OrderDeliveryAddress
   include ActiveModel::Model
-  attr_accessor :p_code, :prefecture_id ,:cities, :address, :building_name, :phone_num
+  attr_accessor :p_code, :prefecture_id ,:cities, :address, :building_name, :phone_num, :user_id, :item_id
 
   with_options presence: true do
     validates :p_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,5 +8,11 @@ class UserDonation
     validates :address
     validates :phone_num , format: { with: /\A[0-9]+\z/, message: "数字を入力してください",length: {maxinum: 11}}
   end
-  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    order =Order.create(user_id: user_id, item_id: item_id)  
+    DeliveryAddress.create(p_code: p_code,prefecture_id: prefecture_id,cities: cities,address: address, building_name: building_name, phone_num: phone_num, order_id: order.id)
+  end
+
 end
