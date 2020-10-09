b5c7c72c39d60c5dc34af0bb359219a8b2eac981
takur-ryu <takuryu.1049@icloud.com>
2020-10-09 15:59:24 +0900

    誤入力取り消し
    勘違いしてmodelではなく、modelテストコードのテストコードにアソシエーションを記載していた為。

--- spec/models/delivery_address_spec.rb
+++ spec/models/delivery_address_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe DeliveryAddress, type: :model do
-  belongs_to :order
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  belongs_to :user
-  belongs_to :item
-  has_one    :delivery_address
 end
