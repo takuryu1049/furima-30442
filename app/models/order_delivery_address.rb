8e16948af365158c75df06c8af2c156a737f240e
takur-ryu <takuryu.1049@icloud.com>
2020-10-09 17:59:04 +0900

    ﾊﾞﾘﾃﾞｰｼｮﾝの設定など/
    orderモデル,derivery_addressモデル
    attr_accessor付与

--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -1,3 +1,12 @@
 class UserDonation
   include ActiveModel::Model
+  attr_accessor :p_code, :prefecture_id ,:cities, :address, :building_name, :phone_num
+
+  with_options presence: true do
+    validates :p_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :cities
+    validates :address
+    validates :phone_num , format: { with: /\A[0-9]+\z/, message: "数字を入力してください",length: {maxinum: 11}}
+  end
+  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
 end
