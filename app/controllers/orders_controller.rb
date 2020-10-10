2d98be65b225411afc177c89079c617755b73c9e
takur-ryu <takuryu.1049@icloud.com>
2020-10-11 02:40:45 +0900

    商品購入ページへのアクセス制限
    一部リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+  before_action :move_to_index
+  
   def index
     @order_delivery_address = OrderDeliveryAddress.new
-    @item = Item.find(params[:item_id])
   end
   
   def create
     @order_delivery_address = OrderDeliveryAddress.new(order_delivery_address_params)
-    @item = Item.find(params[:item_id])
     if @order_delivery_address.valid?
       pay_item
       @order_delivery_address.save
@@ -44,4 +45,15 @@ class OrdersController < ApplicationController
     )
   end
 
+  def move_to_index
+    if user_signed_in?
+      if current_user.id == @item.user.id
+          redirect_to root_path
+      end
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
